8867b933e6d351bdba62528ad1b6fa67bd6e5e49
xi <565372265@qq.com>
2022-10-02 18:32:13 +0800

    fix floam

---
+++ 补充代码/calib/floam_pcd/src/laserProcessingNode_local.cpp
@@ -0,0 +1,150 @@
+// Author of FLOAM: Wang Han 
+// Email wh200720041@gmail.com
+// Homepage https://wanghan.pro
+
+//c++ lib
+#include <cmath>
+#include <vector>
+#include <mutex>
+#include <queue>
+#include <thread>
+#include <chrono>
+#include <fstream>
+
+//ros lib
+#include <ros/ros.h>
+#include <sensor_msgs/Imu.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <nav_msgs/Odometry.h>
+#include <tf/transform_datatypes.h>
+#include <tf/transform_broadcaster.h>
+
+//pcl lib
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/io/pcd_io.h>
+
+//local lib
+#include "lidar.h"
+#include "laserProcessingClass.h"
+
+
+LaserProcessingClass laserProcessing;
+std::mutex mutex_lock;
+std::queue<sensor_msgs::PointCloud2ConstPtr> pointCloudBuf;
+lidar::Lidar lidar_param;
+
+ros::Publisher pubEdgePoints;
+ros::Publisher pubSurfPoints;
+ros::Publisher pubLaserCloudFiltered;
+
+double total_time =0;
+int total_frame=0;
+
+std::string pcd_path = "";
+
+void laser_processing(){
+    std::ifstream fin(pcd_path + "/pcd_timestamp.txt");
+
+    while (!fin.eof())
+    {
+        std::string line;
+        std::getline(fin, line);
+        std::vector<std::string> split_str;
+        boost::split(split_str, line, boost::is_any_of("  ,\t"));
+        
+        pcl::PointCloud<pcl::PointXYZI>::Ptr pointcloud_in(new pcl::PointCloud<pcl::PointXYZI>());
+        pcl::io::loadPCDFile(pcd_path + "/" + split_str[0]+ ".pcd", *pointcloud_in);
+        double timestamp = std::atof(split_str[1].c_str());
+
+        std::vector<std::string> split_timestamp;
+        boost::split(split_timestamp, split_str[1], boost::is_any_of("."));
+
+        while(split_timestamp[1].length() < 9){
+            split_timestamp[1] += "0";
+        }
+
+        ros::Time pointcloud_time = ros::Time(std::stoi(split_timestamp[0].c_str()),std::stoi(split_timestamp[1].c_str()));
+
+        pcl::PointCloud<pcl::PointXYZI>::Ptr pointcloud_edge(new pcl::PointCloud<pcl::PointXYZI>());          
+        pcl::PointCloud<pcl::PointXYZI>::Ptr pointcloud_surf(new pcl::PointCloud<pcl::PointXYZI>());
+
+        std::chrono::time_point<std::chrono::system_clock> start, end;
+        start = std::chrono::system_clock::now();
+        laserProcessing.featureExtraction(pointcloud_in,pointcloud_edge,pointcloud_surf);
+        end = std::chrono::system_clock::now();
+        std::chrono::duration<float> elapsed_seconds = end - start;
+        total_frame++;
+        float time_temp = elapsed_seconds.count() * 1000;
+        total_time+=time_temp;
+        //ROS_INFO("average laser processing time %f ms \n \n", total_time/total_frame);
+
+        sensor_msgs::PointCloud2 laserCloudFilteredMsg;
+        pcl::PointCloud<pcl::PointXYZI>::Ptr pointcloud_filtered(new pcl::PointCloud<pcl::PointXYZI>());  
+        *pointcloud_filtered+=*pointcloud_edge;
+        *pointcloud_filtered+=*pointcloud_surf;
+        pcl::toROSMsg(*pointcloud_filtered, laserCloudFilteredMsg);
+        laserCloudFilteredMsg.header.stamp = pointcloud_time;
+        laserCloudFilteredMsg.header.frame_id = "base_link";
+        pubLaserCloudFiltered.publish(laserCloudFilteredMsg);
+
+        sensor_msgs::PointCloud2 edgePointsMsg;
+        pcl::toROSMsg(*pointcloud_edge, edgePointsMsg);
+        edgePointsMsg.header.stamp = pointcloud_time;
+        edgePointsMsg.header.frame_id = "base_link";
+        pubEdgePoints.publish(edgePointsMsg);
+
+
+        sensor_msgs::PointCloud2 surfPointsMsg;
+        pcl::toROSMsg(*pointcloud_surf, surfPointsMsg);
+        surfPointsMsg.header.stamp = pointcloud_time;
+        surfPointsMsg.header.frame_id = "base_link";
+        pubSurfPoints.publish(surfPointsMsg);
+
+        std::chrono::milliseconds dura(100);
+        std::this_thread::sleep_for(dura);
+    }
+    
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "main");
+    ros::NodeHandle nh;
+
+    int scan_line = 16;
+    double vertical_angle = 2.0;
+    double scan_period= 0.1;
+    double max_dis = 60.0;
+    double min_dis = 2.0;
+
+    nh.getParam("/pcd_path", pcd_path);
+
+    nh.getParam("/scan_period", scan_period); 
+    nh.getParam("/vertical_angle", vertical_angle); 
+    nh.getParam("/max_dis", max_dis);
+    nh.getParam("/min_dis", min_dis);
+    nh.getParam("/scan_line", scan_line);
+
+    lidar_param.setScanPeriod(scan_period);
+    lidar_param.setVerticalAngle(vertical_angle);
+    lidar_param.setLines(scan_line);
+    lidar_param.setMaxDistance(max_dis);
+    lidar_param.setMinDistance(min_dis);
+
+    laserProcessing.init(lidar_param);
+
+    pubLaserCloudFiltered = nh.advertise<sensor_msgs::PointCloud2>("/velodyne_points_filtered", 100);
+
+    pubEdgePoints = nh.advertise<sensor_msgs::PointCloud2>("/laser_cloud_edge", 100);
+
+    pubSurfPoints = nh.advertise<sensor_msgs::PointCloud2>("/laser_cloud_surf", 100); 
+
+    std::thread laser_processing_process{laser_processing};
+
+    ros::spin();
+
+    return 0;
+}
+
